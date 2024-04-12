3ffceb1970f287bb1da001efc0238f3ca862cd9d
Deja Mikayla Boney <155992163+Dboney1@users.noreply.github.com>
2024-04-12 12:51:50 -0400

    Update pricetrakcer.py (Changes)

--- tracker1.py
+++ tracker1.py
@@ -2,53 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
-URL = https://www.amazon.com/Amazon_Fire_HD_10/dp/B0BHZT5S12?ref_=Oct_DLandingS_D_38704265_0
+import html5lib
 
-headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
+URL = input("Enter Amazon URL: ")
 
-def check_price():
-  page = requests.get(URL, headers=headers)
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
-  soup = BeautifulSoup(page.content, 'html.parser')
+offerprice = float(input("Enter Wanted Price: "))
 
-  title = soup.find(id="productTitle").get_text()
-  price = soupfind(id="priceblock_outprice").get_text()
-  converted_price = float(price[0:5])
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login('shopsmartsuite0@gmail.com', 'Georgia2024')
+user_email = input("Enter your email: ")
 
-  if(converted_price < 1.700):
-    send_mail()
+def check_price(URL,Price):
+    page = requests.get(URL, headers=headers)
 
-  print(converted_price)
-  print(title.strip())
+    soup1 = BeautifulSoup(page.content, "html5lib")
 
-  if(converted_price > 1.700):
-    send_mail()
+    soup2 = BeautifulSoup(soup1.prettify(), "html5lib")
 
-def send_mail():
-  server = smtplib.SMTP('smtp.gmail.com', 587)
-  server.ehlo()
-  server.starttls()
-  server.ehlo()
+    title = soup2.find(id="productTitle").get_text().strip()
 
-server.login('
+    price = soup2.find('span', class_='a-offscreen').get_text().strip()
+    priceWithoutSign = price.split("$")[1]
+    floatprice = float(priceWithoutSign)
+    if floatprice <= Price:
+        send_mail(server)
+    print(title)
+    print(floatprice)  
+    
+def send_mail(server):
+    subject = 'Price has dropped!'
+    body = 'Check the amazon link below to see your deal!', URL
+    msg = f"Subject: {subject}\n\n{body}"
+    server.sendmail('shopsmartsuite0@gmail.com',user_email,msg)
+    print('MESSAGE HAS BEEN SENT')
+try:    
+    while(True):    
+        check_price(URL, offerprice)
+        time.sleep(15)
+except KeyboardInterrupt:
+    server.quit()
 
-subject = 'Price fell down!'
-body = 'Check out the amazon link https://www.amazon.com/Amazon_Fire_HD_10/dp/B0BHZT5S12?ref_=Oct_DLandingS_D_38704265_0
 
-msg = f"Subject: {subject}\n\n{body}"
-
-server.sendmail(
-
-
-)
-print('HEY EMAIL HAS BEEN SENT!')
-
-server.quit()
-
-
-while(True):
-  check_price()
-  time.sleep(60 * 60)
 
 
 
