3cf620e6b8292a7be335d67887501a48ab786715
libbyfox70 <156025280+libbyfox70@users.noreply.github.com>
2024-04-11 22:19:11 -0400

    Update pricetracker.py

--- pricetracker.py
+++ pricetracker.py
@@ -15,32 +15,40 @@ class AmazonPriceTracker:
         self.receiver_email = receiver_email
 
 #define the URL and create user agent info
+    def get_product_info(self):
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"}
+        page = requests.get(self.url, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        title = soup.find(id="productTitle").get_text().strip
+        price = soup.find(id="priceblock_ourprice").get_text()
+        converted_price = float(price[1:].replace(',', ''))
+        return title, converted_price
 
-URL = 'https://www.amazon.com/dp/B08PZHYWJS/ref=fs_a_mdt2_us3'
+#send email if the price drops below the target price
+    def send_notification(self, title, price):
+        subject = f'Price Dropped for {title}'
+        body = f'The price has dropped to ${price}!\nCheck it out here: {self.url}'
+        message = f'Subject: {subject}\n\n{body}'
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login(self.sender_email, self.send_password)
+            smtp.sendmail(self.sender_email, self.receiver_email, message)
+        
+#track price of the product and sends notification if price drops below the target price
+    def track_price(self)
+        while True:
+            title, price = self.get_product_info()
+            print(f"Current Price for '{title}': ${price}")
+            if price <= self.target_price:
+                print(f"Price dropped below ${self.target_price}! But now!")
+                self.send_notification(title, price)
+                break
+            time.sleep(3600)
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"}
 
-page = requests.get(URL, headers=headers)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-title = soup.find(id="productTitle").get_text()
-price = soup.find(id="priceblock_ourprice").get_text()
-converted_price = float(price[0:3])
-
-if(converted_price < 545): 
-    send_mail()
-    
-print(converted_price)
-print(title.strip())
-
-if(converted_price > 545):
-    send_mail()
-def send_mail():
-    server = smtplib.STMP('smtp.gmail.com',587)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
+        
 
     server.login('dejaboney1025@gmail.com',' lovecupcakes1')
 
