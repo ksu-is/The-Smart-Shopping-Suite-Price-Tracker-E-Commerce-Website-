321ee4928a00cb2610add23ae417ca4f37de893d
libbyfox70 <156025280+libbyfox70@users.noreply.github.com>
2024-04-20 22:07:42 -0400

    Update pricetracker.py

--- pricetracker.py
+++ pricetracker.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
+
 URL = input("Enter Amazon URL: ")
 offer_price = float(input("Enter Wanted Price: "))
-user_email = input("Enter your email:) 
+user_email = input("Enter your email: ") 
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
@@ -14,21 +15,24 @@ headers = {
     "Upgrade-Insecure-Requests": "1"
 }
 
-    def send_mail(url):
+def send_mail(url):
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.ehlo()
-        server.login('shopsmartsuite.com', 'Georgia2024')
+        server.login('shopsmartsuite0@gmail.com', 'Georgia2024')
 
+        shopsmartsuite_email = 'shopsmartsuite0@gmail.com
+        gmail_domain = 'gmail.com'
+        
         subject = 'Price Has Dropped!'
         body = f'Check the Amazon link below to see your deal: {URL}
         msg = f"Subject: {subject}\n\n{body}"
-        server.sendmail('shopsmartsuite0@gmail.com', user_email, msg)
+        server.sendmail(f'shopsmartsuite0@gmail.com', user_email, msg)
         print('MESSAGE HAS BEEN SENT')
         server.quit()
         
-    def check_price(URL, Price):
+def check_price(URL, Price):
         page = requests.get(URL, headers=headers) 
         soup = BeautifulSoup(page.content, 'htmlparser')
 
@@ -47,11 +51,14 @@ headers = {
             if price <= Price:
                 send_mail()
         
-    try:
+try:
         while True:
             check_price(URL, offer_price)
             time.sleep(60 * 60)
-    except KeyboardInterrupt:
+except KeyboardInterrupt:
         pass
+
+
+  
 
         
